9ae2c84837d7ba65ef3e46360ca3b2258425f0f7
Matthew Stronge <matthew.stronge@gmail.com>
2026-02-25 02:49:27 +0000

    Fixing jsdom dep issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["jsdom", "@mozilla/readability"],
+  /* config options here */
 };
 
 export default nextConfig;
